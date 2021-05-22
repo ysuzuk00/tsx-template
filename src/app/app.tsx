ed5b14f2d6e2e4c00e595bf00fc5b37abeb62a2c
yusuzuk00 <yutadx.szk@gmail.com>
2021-05-22 15:53:43 +0900

    first commit

---
+++ src/app/app.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+const Hello: React.FunctionComponent<{ compiler: string, framework: string }> = (props) => {
+  return (
+    <div>
+      <div>{props.compiler}</div>
+      <div>{props.framework}</div>
+      <div>nasu</div>
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <Hello compiler="TypeScript" framework="React" />,
+  document.getElementById("root")
+);
